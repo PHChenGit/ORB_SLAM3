046bd1e5e912b781fdfa9ba912d20de8dd30eab3
PHChenGit <chenpohsun1@gmail.com>
2023-10-13 08:45:24 +0800

    feat: Add a script to run docker

---
+++ run.sh
@@ -0,0 +1,16 @@
+xhost +local:root
+
+XAUTH=/tmp/.docker.xauth
+
+docker run -it \
+    --env="DISPLAY=unix$DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --env="XAUTHORITY=$XAUTH" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --volume="$XAUTH:$XAUTH" \
+    --net=host \
+    --privileged \
+    tokohsun/orb-slam3:1.0.0 \
+    /bin/bash
+
+echo "Done!!"
