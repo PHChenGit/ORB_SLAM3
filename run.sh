f119b12de5048bb466484f3262070104ce2b8ad1
PHChenGit <chenpohsun1@gmail.com>
2023-10-15 09:56:00 +0800

    chore: Enable GPUs

--- run.sh
+++ run.sh
@@ -8,8 +8,10 @@ docker run -it \
     --env="XAUTHORITY=$XAUTH" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     --volume="$XAUTH:$XAUTH" \
+    --volume="$(pwd):/orb_slam3/" \
     --net=host \
     --privileged \
+    --gpus all \
     tokohsun/orb-slam3:1.0.0 \
     /bin/bash
 
